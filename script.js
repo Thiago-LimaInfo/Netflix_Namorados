5d7189ca89f8984332c2d65a601ea3d0c2b1a48c
gomestizado <matheus_mgc@yahoo.com.br>
2025-10-29 16:17:47 -0300

    add buttons netflix

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+const openBtn = document.getElementById('openPopup');
+const closeBtn = document.getElementById('closePopup');
+const popup = document.getElementById('popup');
+
 const nav = document.getElementById('nav');
+
 window.addEventListener('scroll', ()=>{
     if(window.scrollY >= 400){
         nav.classList.add('nav__black');
@@ -6,3 +11,16 @@ window.addEventListener('scroll', ()=>{
         nav.classList.remove('nav__black');
     }
 })
+
+openBtn.addEventListener('click', () => {
+    popup.style.display = 'flex';
+  });
+
+  closeBtn.addEventListener('click', () => {
+    popup.style.display = 'none';
+  });
+
+  // Fechar ao clicar fora
+  window.addEventListener('click', (e) => {
+    if (e.target === popup) popup.style.display = 'none';
+  });
